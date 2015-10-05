834fca9e4705d241e2ca3c4329055195aeafbbf8
Brad Clark <bdashrad@gmail.com>
2015-10-05 12:10:15 -0400

    separate gem loading

--- logwatch.rb
+++ logwatch.rb
@@ -1,21 +1,32 @@
 require 'rbconfig'
 include RbConfig
 
+def warm_up
+  @os = RbConfig::CONFIG['host_os'] # string with OS name
+  load_os_gems
+end
+
+def load_os_gems
+  case @os
+  when (/bsd|darwin/)
+    require 'rb-kqueue'
+  when (/linux/)
+    require 'rb-inotify'
+  end
+end
+
 # open the file and watch with inotify
-def tail_dash_f(filename)
+def tail_file(filename)
   open(filename) do |file|
     file.seek(0, IO::SEEK_END)
-    # string with OS name, like "amd64-freebsd8"
-    case RbConfig::CONFIG['host_os']
+    case @os
     when (/bsd|darwin/)
-      require 'rb-kqueue'
       queue = KQueue::Queue.new
       queue.watch_file(filename, :extend) do
         yield file.read
       end
       queue.run
     when (/linux/)
-      require 'rb-inotify'
       queue = INotify::Notifier.new
       queue.watch(filename, :modify) do
         yield file.read
@@ -32,9 +43,12 @@ def tail_dash_f(filename)
   end
 end
 
-tail_dash_f(ARGV.first) do |data|
+warm_up
+
+tail_file(ARGV.first) do |data|
   puts data
   if data =~ /error/i
-  puts "ERROR"
+    puts 'ERROR'
+    puts 'more coming'
   end
 end
