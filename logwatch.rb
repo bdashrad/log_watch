5c2a34965987e314d4116508249f4dd9755a6565
Brad Clark <bdashrad@gmail.com>
2015-10-05 12:34:17 -0400

    add logwatch class

--- logwatch.rb
+++ logwatch.rb
@@ -1,51 +1,53 @@
 require 'rbconfig'
 include RbConfig
 
-def warm_up
-  @os = RbConfig::CONFIG['host_os'] # string with OS name
-  load_os_gems
-end
-
-def load_os_gems
-  case @os
-  when (/bsd|darwin/)
-    require 'rb-kqueue'
-  when (/linux/)
-    require 'rb-inotify'
+# watch w3c logs and alert on chagnes
+class LogWatch
+  def initialize
+    @os = RbConfig::CONFIG['host_os'] # string with OS name
+    load_os_gems
   end
-end
 
-# open the file and watch with inotify
-def tail_file(filename)
-  open(filename) do |file|
-    file.seek(0, IO::SEEK_END)
+  def load_os_gems
     case @os
     when (/bsd|darwin/)
-      queue = KQueue::Queue.new
-      queue.watch_file(filename, :extend) do
-        yield file.read
-      end
-      queue.run
+      require 'rb-kqueue'
     when (/linux/)
-      queue = INotify::Notifier.new
-      queue.watch(filename, :modify) do
-        yield file.read
-      end
-      queue.run
-    else
-      loop do
-        changes = file.read
-        unless changes.empty? yield changes
+      require 'rb-inotify'
+    end
+  end
+
+  # open the file and watch with inotify
+  def tail_file(filename)
+    open(filename) do |file|
+      file.seek(0, IO::SEEK_END)
+      case @os
+      when (/bsd|darwin/)
+        queue = KQueue::Queue.new
+        queue.watch_file(filename, :extend) do
+          yield file.read
         end
-        sleep 1.0
+        queue.run
+      when (/linux/)
+        queue = INotify::Notifier.new
+        queue.watch(filename, :modify) do
+          yield file.read
+        end
+        queue.run
+      else
+        loop do
+          changes = file.read
+          unless changes.empty? yield changes
+          end
+          sleep 1.0
+        end
       end
     end
   end
 end
 
-warm_up
-
-tail_file(ARGV.first) do |data|
+log = LogWatch.new
+log.tail_file(ARGV.first) do |data|
   puts data
   if data =~ /error/i
     puts 'ERROR'
