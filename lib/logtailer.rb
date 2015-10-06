53cd4568e51c654e33c99db0390427e588bc89f0
Brad Clark <bdashrad@gmail.com>
2015-10-06 17:01:43 -0400

    clean up dynamic gem loading, break os tailing into classes

--- lib/logtailer.rb
+++ lib/logtailer.rb
@@ -1,21 +1,14 @@
 require 'rbconfig'
 include RbConfig
 
+require 'rb-kqueue' if RbConfig::CONFIG['host_os'] =~ (/bsd|darwin/i)
+require 'rb-inotify' if RbConfig::CONFIG['host_os'] =~ (/linux/i)
+
 module LogTailer
   # watch w3c logs and alert on chagnes
   class LogTail
     def initialize
-      @os = RbConfig::CONFIG['host_os'] # string with OS name
-      load_os_gems
-    end
-
-    def load_os_gems
-      case @os
-      when (/bsd|darwin/)
-        require 'rb-kqueue'
-      when (/linux/)
-        require 'rb-inotify'
-      end
+      # things and stuff
     end
 
     # open the file and watch for changes
@@ -42,6 +35,48 @@ module LogTailer
             yield changes unless changes.empty?
             sleep 1.0
           end
+        end
+      end
+    end
+  end
+
+  # Tail files in BSD with kqueue
+  class BSDTail
+    def tail_file(filename)
+      open(filename) do |file|
+        file.seek(0, IO::SEEK_END)
+        queue = KQueue::Queue.new
+        queue.watch_file(filename, :extend) do
+          yield file.read
+        end
+        queue.run
+      end
+    end
+  end
+
+  # Tail files in Linux with inotify
+  class LinuxTail
+    def tail_file(filename)
+      open(filename) do |file|
+        file.seek(0, IO::SEEK_END)
+        queue = INotify::Notifier.new
+        queue.watch(filename, :modify) do
+          yield file.read
+        end
+        queue.run
+      end
+    end
+  end
+
+  # tail files when inotify/kqueue are not available
+  class DefaultTail
+    def tail_file(filename)
+      open(filename) do |file|
+        file.seek(0, IO::SEEK_END)
+        loop do
+          changes = file.read
+          yield changes unless changes.empty?
+          sleep 1.0
         end
       end
     end
