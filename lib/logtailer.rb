963a0a9a4de688a8ed181e87daceea32e58a5e02
Brad Clark <bdashrad@gmail.com>
2015-10-06 17:24:03 -0400

    remove debugging lines

--- lib/logtailer.rb
+++ lib/logtailer.rb
@@ -3,10 +3,8 @@ include RbConfig
 
 case RbConfig::CONFIG['host_os']
 when (/bsd|darwin/)
-  puts 'bsd'
   require 'rb-kqueue'
 when (/linux/)
-  puts 'linux'
   require 'rb-inotify'
 end
 
@@ -14,7 +12,7 @@ module LogTailer
   # watch w3c logs and alert on chagnes
   class LogTail
     def initialize
-      # things and stuff
+      @os = RbConfig::CONFIG['host_os']
     end
 
     # open the file and watch for changes
@@ -22,23 +20,20 @@ module LogTailer
     def tail_file(filename)
       open(filename) do |file|
         file.seek(0, IO::SEEK_END)
-        case RbConfig::CONFIG['host_os']
+        case @os
         when (/bsd|darwin/i)
-          puts 'kqueue tail'
           queue = KQueue::Queue.new
           queue.watch_file(filename, :extend) do
             yield file.read
           end
           queue.run
         when (/linux/)
-          puts 'inotify tail'
           queue = INotify::Notifier.new
           queue.watch(filename, :modify) do
             yield file.read
           end
           queue.run
         else
-          puts 'default tail'
           loop do
             changes = file.read
             yield changes unless changes.empty?
