797bdfa516e0f6df036d1a4d4bd6071b17b45f11
Brad Clark <bdashrad@gmail.com>
2015-10-13 00:50:46 -0400

    add so many comments

--- lib/log_watch/monitor.rb
+++ lib/log_watch/monitor.rb
@@ -15,14 +15,12 @@ module LogWatch
         (?<status>\d+)\s
         (?<bytes>\S+)
       /x
-      # put loglines into empty array
-      @loglines = []
-      @counter = []
-      @alert = false
-      @threshold = 6
-      @total_hits = 0
-      @alertqueue = []
-      @section_hits = Hash.new { |h, k| h[k] = 0 }
+      @alert = false # we start off not alerting
+      @alertqueue = [] # a place to hold all of our alerts
+      @counter = [] # how many hits in the last 2 min
+      @loglines = [] # put loglines into empty array
+      @threshold = 6 # how many hits per 2 min is ok
+      @total_hits = 0 # how many total hits since we started
     end
 
     def start(filename)
@@ -52,10 +50,14 @@ module LogWatch
 
     def count_hits
       loop do
+        # lets look what we got so far
         @loglines.each do |log|
+          # add count to each section
           @section_hits[log['section']] += 1
         end
+        # if we have hits show some stats
         show_stats(@section_hits) unless @section_hits.length == 0
+        # reset current hits
         @loglines = []
         sleep 10
       end
@@ -82,9 +84,11 @@ module LogWatch
     end
 
     def check_alert
+      # if we're not in alert mode and cross the threshold we gotta know
       if @counter.length >= @threshold && @alert == false
         @alert = true
         alert_traffic(@counter.length)
+      # if we're in alert mode and drop to ok again tell us
       elsif @counter.length < @threshold && @alert == true
         @alert = false
         alert_recovery(@counter.length)
@@ -92,28 +96,33 @@ module LogWatch
     end
 
     def alert_traffic(hits)
+      # push a crit into the alerts array and
+      # print latest alert so we don't miss any
       t = Time.now.getutc
       a = "#{t} | High traffic generated an alert - hits = #{hits}"
-      # p a
       @alertqueue.push(a)
       puts @alertqueue.last
     end
 
     def alert_recovery(hits)
+      # push a recovery into the alerts array and
+      # print latest alert so we don't miss any
       t = Time.now.getutc
       a = "#{t} | High traffic event over - hits = #{hits}"
-      # p a
       @alertqueue.push(a)
       puts @alertqueue.last
     end
 
     def show_alerts
+      # print all alerts we've had so far
       @alertqueue.each do |alert|
         puts alert
       end
     end
 
     def show_stats(hits)
+      # shuold probably do this all in ncurses or something
+      # instead of clearing the screen and printing it all over again
       system 'clear'
       # top 5 sections
       top_hits = hits.sort_by { |_, v| v }.reverse.first(5).to_h
